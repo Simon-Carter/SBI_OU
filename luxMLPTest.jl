4cba0e0430177325f55ba83a517139ee76e8e747
Simon-Carter <simondcarter@hotmail.com>
2023-11-06 15:14:38 -0500

    + loss function and generated training state

--- luxMLPTest.jl
+++ luxMLPTest.jl
@@ -1,5 +1,6 @@
 using Lux, Optimisers, Random, Zygote
 
+include("loss_function.jl")
 
 #generate random number generator
 rng = MersenneTwister()
@@ -8,6 +9,8 @@ Random.seed!(rng, 12345)
 # set the optimiser model
 opt = Adam(0.03f0)
 
-
 # st the architecture of the neural network
 model = Chain(Dense(3 => 4, relu), Dense(4 => 6))
+
+tstate = Lux.Training.TrainState(rng, model, opt);
+
