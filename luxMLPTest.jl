6714478b4f2445d1ce1af1bb849783c70e4bc109
Simon-Carter <simondcarter@hotmail.com>
2023-11-07 17:20:42 -0500

    Aded neural network training pipeline

--- luxMLPTest.jl
+++ luxMLPTest.jl
@@ -1,6 +1,7 @@
-using Lux, Optimisers, Random, Zygote
+using Lux, Optimisers, Random, Zygote, ADTypes
 
 include("loss_function.jl")
+include("./TestingScripts/test_data_gen.jl")
 
 #generate random number generator
 rng = MersenneTwister()
@@ -14,3 +15,21 @@ model = Chain(Dense(3 => 4, relu), Dense(4 => 6))
 
 tstate = Lux.Training.TrainState(rng, model, opt);
 
+vjp_rule = Lux.Training.AutoZygote()
+ADTypes.AutoZygote()
+
+function main(tstate::Lux.Experimental.TrainState, vjp, data, epochs)
+    for epoch in 1:epochs
+        grads, loss, stats, tstate = Lux.Training.compute_gradients(vjp,
+        lux_gaussian_made_loss, data, tstate)
+        println("Epoch: $(epoch) || Loss: $(loss)")
+        tstate = Lux.Training.apply_gradients(tstate, grads)
+    end
+    return tstate
+end
+
+dev_cpu = cpu_device()
+dev_gpu = gpu_device()
+
+tstate = main(tstate, vjp_rule, gen_data, 50000)
+y_pred = dev_cpu(Lux.apply(tstate.model, gen_data, tstate.parameters, tstate.states)[1])
