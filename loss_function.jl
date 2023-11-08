aeb6ebeb7fed0f1b1b2af7f039018a0b9e904ea8
Simon-Carter <simondcarter@hotmail.com>
2023-11-08 10:15:09 -0500

    fixed bug with wrong sing in exp - loss function Added an aditional term to try and keep variance consistant TODO -  check if mean makes a difference               - check if original loss function used this

--- loss_function.jl
+++ loss_function.jl
@@ -31,12 +31,11 @@ function log_std_loss(y_pred, data)
     n = div(size(y_pred)[1], 2)
     half1 = @view y_pred[1:n,:]
     half2 = @view y_pred[n+1:end,:]
-    u = (data-half1).*exp.(half2)
-    negloglike = 0.5*log(2*pi) .+ 0.5.*u.^2
+    u = (data.-half1).*exp.(-half2)
+    negloglike = 0.5*log(2*pi) .+ 0.5.*(u.^2) .+ half2
     negloglike = sum(negloglike)
     return negloglike
 end
-
 
 
 # for lux.jl loss function needts to take 4 parameter,  and return 3 parameters
