3eaf13909fe3ec65303553c6cfd165f105ced1e4
Simon-Carter <simondcarter@hotmail.com>
2023-11-21 20:53:17 -0500

    Added Masked Layer custom layer curretly it is just a simple dense layer

---
+++ Masked_layer.jl
@@ -0,0 +1,74 @@
+
+"""
+    Dense(in_dims => out_dims, activation=identity; init_weight=glorot_uniform,
+          init_bias=zeros32, bias::Bool=true)
+
+Create a traditional fully connected layer, whose forward pass is given by:
+`y = activation.(weight * x .+ bias)`
+
+## Arguments
+
+  - `in_dims`: number of input dimensions
+  - `out_dims`: number of output dimensions
+  - `activation`: activation function
+
+## Keyword Arguments
+
+  - `init_weight`: initializer for the weight matrix
+    (`weight = init_weight(rng, out_dims, in_dims)`)
+  - `init_bias`: initializer for the bias vector (ignored if `use_bias=false`)
+  - `use_bias`: Trainable bias can be disabled entirely by setting this to `false`
+  - `allow_fast_activation`: If `true`, then certain activations can be approximated with
+    a faster version. The new activation function will be given by
+    `NNlib.fast_act(activation)`
+
+## Input
+
+  - `x` must be an AbstractArray with `size(x, 1) == in_dims`
+
+## Returns
+
+- AbstractArray with dimensions `(out_dims, ...)` where `...` are the dimensions of `x`
+- Empty `NamedTuple()`
+
+## Parameters
+
+- `weight`: Weight Matrix of size `(out_dims, in_dims)`
+- `bias`: Bias of size `(out_dims, 1)` (present if `use_bias=true`)
+"""
+@concrete struct MaskedLinear <: Lux.AbstractExplicitLayer
+  in_dims::Int
+  out_dims::Int
+  init_weight
+  init_bias
+  mask
+end
+
+function Base.show(io::IO, d::MaskedLinear)
+  print(io, "MaskedLinear($(d.in_dims) => $(d.out_dims)")
+  return print(io, ")")
+end
+
+function MaskedLinear(mapping::Pair{<:Int, <:Int}; kwargs...)
+  return Dense(first(mapping), last(mapping); kwargs...)
+end
+
+function MaskedLinear(in_dims::Int, out_dims::Int; init_weight=glorot_uniform,
+        init_bias=zeros32)
+    return MaskedLinear(in_dims, out_dims, init_weight, init_bias)
+end
+
+function initialparameters(rng::AbstractRNG, d::MaskedLinear)
+    return (weight=d.init_weight(rng, d.out_dims, d.in_dims),
+        bias=d.init_bias(rng, d.out_dims, 1))
+end
+
+function parameterlength(d::MaskedLinear)
+    return d.out_dims * (d.in_dims + 1)
+end
+
+statelength(d::MaskedLinear) = 0
+
+@inline function (d::MaskedLinear)(x::AbstractVecOrMat, ps, st::NamedTuple)
+    return ps.weight * x + ps.bias, st
+end
