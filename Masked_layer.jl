ff5e6259b49e1ec08b5abb40195e47b51139e851
Simon-Carter <simondcarter@hotmail.com>
2023-12-11 16:53:44 -0500

    Added MAF as a chain of MADES (untested)

--- Masked_layer.jl
+++ Masked_layer.jl
@@ -79,12 +79,18 @@ Lux.statelength(d::MaskedLinear) = 0
     return d.activation.(((d.init_mask[]).*ps.weight)*x .+ ps.bias), st
 end
 
+# stuff
+
+struct MADE{T <: NamedTuple} <: Lux.AbstractExplicitContainerLayer{(:layers,)}
+  layers::T
+  mask::Base.RefValue{}
+end
 
 function sample(T::MADE, ps, st)
   input = T.layers[1].in_dims
   samples = rand(input)
   println(samples)
-  for i in 2:input
+  for i in 1:input
     mean = T(samples, ps, st)[1][i]
     std = exp(T(samples, ps, st)[1][i+input])
     samples[i] = std*rand() + mean
@@ -92,12 +98,6 @@ function sample(T::MADE, ps, st)
   return samples
 end
 
-# stuff
-
-struct MADE{T <: NamedTuple} <: Lux.AbstractExplicitContainerLayer{(:layers,)}
-  layers::T
-  mask::Base.RefValue{}
-end
 
 function generate_m_k(layers)
   dims = [(i.in_dims, i.out_dims) for i in layers]
@@ -176,6 +176,56 @@ end
 
 MADE(; kwargs...) = MADE((; kwargs...))
 
+
+
+
+
+
+
+# MAF layer (chain of MADE)
+
+
+struct MAF{T <: NamedTuple} <: Lux.AbstractExplicitContainerLayer{(:layers,)}
+  layers::T
+end
+
+function MAF(layers...;)
+  names = ntuple(i -> Symbol("MADE_$i"), length(layers))
+  return MAF(NamedTuple{names}(layers))
+end
+
+(c::MAF)(x, ps, st::NamedTuple) = applyMAF(c.layers, x, ps, st)
+
+@inline function coord_transform(x, y_pred)
+    n = div(size(y_pred)[1], 2)
+    half1 = @view y_pred[1:n,:]
+    half2 = @view y_pred[n+1:end,:]
+  return (x .- half1).*exp.(-half2)
+end
+
+@generated function applyMAF(layers::NamedTuple{fields}, x, ps,
+  st::NamedTuple{fields}) where {fields}
+N = length(fields)
+x_symbols = vcat([:x], [gensym() for _ in 1:N])
+st_symbols = [gensym() for _ in 1:N]
+calls1 = [:(($(x_symbols[i + 1]), $(st_symbols[i])) = Lux.apply(layers.$(fields[i]),
+  $(x_symbols[i]), ps.$(fields[i]), st.$(fields[i]))) for i in 1:N]
+calls2 = [:($(x_symbols[i]) = coord_transform($(x_symbols[i-1]),$(x_symbols[i]))) for i in 2:N+1]
+
+
+n = length(calls1) + length(calls2)
+calls = similar(calls1, n)
+
+calls[1:2:n] .= calls1
+calls[2:2:n] .= calls2
+
+push!(calls, :(st = NamedTuple{$fields}((($(Tuple(st_symbols)...),)))))
+push!(calls, :(return $(x_symbols[N + 1]), st))
+return Expr(:block, calls...)
+end
+
+
+
 #=
 #quick test of the set_mask in applyMADE
 @generated function test(layers::NamedTuple{fields}, masks) where {fields}
@@ -185,3 +235,7 @@ MADE(; kwargs...) = MADE((; kwargs...))
   return Expr(:block, calls_mask...)
 end
 =#
+
+@generated function test()
+  return :(coord_transform($,$))
+end
