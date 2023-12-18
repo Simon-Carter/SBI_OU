1644f3428f3fa4efd9a4d306c343433c9040f5d9
Simon-Carter <simondcarter@hotmail.com>
2023-12-18 16:04:17 -0500

    simple MADE test function

---
+++ MADE_simple_test.jl
@@ -0,0 +1,44 @@
+using Lux, Optimisers, Random, Zygote, ADTypes, LinearAlgebra, ConcreteStructs, OneHotArrays
+
+
+include("loss_function.jl")
+include("./TestingScripts/test_data_gen.jl")
+include("./Masked_layer.jl")
+
+import MLDatasets: MNIST
+
+import MLUtils: DataLoader, splitobs
+
+#generate random number generator
+rng = MersenneTwister()
+Random.seed!(rng, 12345)
+
+# set the optimiser model
+opt = Adam(0.060)
+
+model = MADE(MaskedLinear(2, 3, relu), MaskedLinear(3, 4, relu), MaskedLinear(4, 6, relu), MaskedLinear(6, 5, relu), MaskedLinear(5,4))
+
+tstate = Lux.Training.TrainState(rng, model, opt);
+
+
+train_dataloader = generate_data_maf_paper(20000,5000)
+
+vjp_rule = Lux.Training.AutoZygote()
+ADTypes.AutoZygote()
+
+function main(tstate::Lux.Experimental.TrainState, vjp, data_loader, epochs)
+    for epoch in 1:epochs
+        for data in data_loader
+            grads, loss, stats, tstate = Lux.Training.compute_gradients(vjp,
+            lux_gaussian_made_loss, data, tstate)
+            println("Epoch: $(epoch) || Loss: $(loss)")
+            tstate = Lux.Training.apply_gradients(tstate, grads)
+        end
+    end
+    return tstate
+end
+
+dev_cpu = cpu_device()
+dev_gpu = gpu_device()
+
+tstate = main(tstate, vjp_rule, train_dataloader, 250)
